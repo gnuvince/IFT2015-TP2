8893d09b0bec65c14f62566101d61355e070f455
Vincent Foley <vfoley@gmail.com>
2010-11-02 00:22:50 -0400

    Added some drawings

--- src/plante/TurtleState.java
+++ src/plante/TurtleState.java
@@ -3,24 +3,29 @@
  * Stella Domingo (DOMC03588902)
  */
 
-
 package plante;
 
+/**
+ * @author vince The state of a turtle is a 3-tuple containing the x and y
+ *         coordinates as well as the current angle.
+ */
 public class TurtleState implements Cloneable {
     public int x;
     public int y;
     public double angle;
-    
+
     public TurtleState(int x, int y, double angle) {
         this.x = x;
         this.y = y;
         this.angle = angle;
     }
-    
+
     public String toString() {
         return "(" + x + ", " + y + ", " + angle + ")";
     }
-    
+
+    // Create a new copy of the current state; used for pushing/popping states
+    // from a stack of states.
     public TurtleState clone() {
         return new TurtleState(x, y, angle);
     }
